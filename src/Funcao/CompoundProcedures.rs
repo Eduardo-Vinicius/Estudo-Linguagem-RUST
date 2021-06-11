79e2b460b7b965ea61b98599ba78ffff1267975e
eduardo-vinicius <eduardo.vinicius01@outlook.com>
2021-06-10 22:25:38 -0300

    procedure e compound

---
+++ src/Funcao/CompoundProcedures.rs
@@ -0,0 +1,28 @@
+fn main() {
+    #[derive(Debug)]
+    struct Retangulo {
+        altura: i64,
+        base: i64,
+    }
+
+    let retangulo1 = Retangulo {
+        altura: 10,
+        base: 15,
+    };
+
+    let retangulo2 = Retangulo {
+        altura: 10,
+        base: 15,
+    };
+
+    fn e1_area_retangulo(a: i64, b: i64) -> i64 {
+        return a * b;
+    }
+
+    fn e2_retangulos_iguais(r1: Retangulo, r2: Retangulo) -> bool {
+        return e1_area_retangulo(r1.altura, r1.base) == e1_area_retangulo(r2.altura, r2.base);
+    }
+
+    let x: bool = e2_retangulos_iguais(retangulo1, retangulo2);
+    println!("{}", x)
+}
