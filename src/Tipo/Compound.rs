e3992074146908d364bf38029b9f23fdde100816
barretot <barretoruan77@gmail.com>
2021-06-15 12:28:21 -0300

    fila, pilha e dicionario

--- src/Tipo/Compound.rs
+++ src/Tipo/Compound.rs
@@ -1,5 +1,3 @@
-
-
 struct Pessoa{
     nome:String;
 }
--- src/Tipo/Recursive.rs
+++ src/Tipo/Recursive.rs
@@ -1,3 +1,4 @@
+Use std::collections::HashMap;​
 
 fn e1_Vetor(){
     //
@@ -40,10 +41,53 @@ fn e2_Lista(){
 }
 
 fn e3_Fila(){
-    let fila: VecDeque(u32) = VecDeque::new();
-    fila.push_back(10);
-    fila.push_back(20);
-    fila.push_back(30);
+    let fila: VecDeque(u64) = VecDeque::new();
+    fila.push_front(10);
+    fila.push_front(20);
+    fila.push_front(30);
 
+    // Remove
+    let a = fila.pop_front();​
+    let b = fila.pop_front();​
+    let c = fila.pop_front();
+
+    let cumprimento = fila.len();
     
+}
+
+fn e4_Pilha() {
+    let pilha: VecDeque(u64) = VecDeque::new();
+    pilha.push_back(10);
+    pilha.push_back(20);
+    pilha.push_back(30);
+
+    // Remove
+    let a = pop_back();​
+    let b = pop_back();​
+    let c = pop_back(); 
+
+    let cumprimento = pilha.len();
+}
+
+fn e5_Dicionarios(){
+    
+    let mut dicionario: HashMap<i64, &str> = HashMap::new();​
+
+    // Inserindo
+    dicionario.insert(1, "C#");​
+    dicionario.insert(2, "Javascript");​
+    dicionario.insert(3, "Python");
+
+    // Lendo cada um
+    let a = dicionario.get(1);​
+    let b = dicionario.get(2);​
+    let c = dicionario.get(3);
+
+    // Iterando
+    let x = dic.iter();
+
+    // Update
+    dicionario.insert(1, "Java");
+
+    dicionario.remove(&1);
 }
